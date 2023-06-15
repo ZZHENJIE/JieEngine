83d9bc140d3987295f7dc92e2d4e79cd59918881
ZHONGZHENJIE <508916879@qq.com>
2023-06-15 11:20:57 +0800

    Update

--- MainMapStruct.h
+++ MainMapStruct.h
@@ -2,7 +2,6 @@
 #define MAINMAPSTRUCT_H
 
 #include "Include/MapStruct.h"
-#include "TestMapStruct.h"
 
 class MainMapStruct : public MapStruct{
     public:
@@ -22,10 +21,7 @@ class MainMapStruct : public MapStruct{
             接收事件 除了退出事件 其它事件都接收的到
         */
         void WindowEvent(SDL_Event Event,MapStruct * MWindowMapStruct){
-            if(Event.button.clicks == 1){
-                MWindowMapStruct->Free();
-                MWindowMapStruct = new TestMapStruct();
-            }
+
         }
         /*
             释放资源函数
